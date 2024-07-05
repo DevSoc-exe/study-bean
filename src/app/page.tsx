9aea98fe852291d3a7e4cf848fcc3cbf73ab8abc
charankamal20 <charanksingh20@gmail.com>
2024-07-06 03:46:15 +0530

    Adding remote work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-
-    </>
+    <div className="h-full w-full flex justify-center items-center gap-4">
+      <Link href="/auth">Auth</Link>
+      <Link href="/home">Home</Link>
+      <Link href="/usernmae/todos">Todo</Link>
+      <Link href="/usernmae/pomodoro">Pomodoro</Link>
+    </div>
   );
 }
--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { TodoPriority } from "@/types/ToDo";
 
 export const LoginSchema = z.object({
   email: z.string().email({
@@ -28,4 +29,13 @@ export const RegisterFormSchema = z.object({
       required_error: "This field cannot be empty",
     })
     .min(5),
+});
+
+export const TodoSchema = z.object({
+  todo: z.string().min(1, { message: "Todo body cannot be null" }),
+  priority: z.nativeEnum(TodoPriority, {
+    invalid_type_error: "priority must be of type `TodoPriority`",
+    required_error:
+      "priority value must be set to either of [High, Medium, Low]",
+  }),
 });
