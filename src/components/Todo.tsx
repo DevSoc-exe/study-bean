14cc3db401fe5773a4c10ed58b475ea5315fefd7
Kanishk Nagpal <kaynagpal03@gmail.com>
2024-07-11 00:44:28 +0530

    updated components

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,8 +1,9 @@
 import { Check, Edit, Pencil, Trash, Trash2, X } from 'lucide-react';
 import React from 'react'
 import Icon from './Icon';
-import { TodoPriority } from '@/types/ToDo';
+import { TodoPriority, todoPriorityColorCode } from '@/types/ToDo';
 import { PriorityDropDwon } from './PriorityDropDown';
+import { ViewTodoDialog } from './ViewTodoDialog';
 
 
 const Todo = (
@@ -26,24 +27,11 @@ const Todo = (
     }
 
 ) => {
-    let priorityBGColor = ""
-    switch (priority) {
-        case TodoPriority.High:
-            priorityBGColor = "bg-red-100"
-            break;
-        case TodoPriority.Medium:
-            priorityBGColor = "bg-orange-100"
-            break;
-        case TodoPriority.Low:
-            priorityBGColor = "bg-yellow-100"
-            break;
+    let priorityBGColor = todoPriorityColorCode[priority]
 
-        default:
-            break;
-    }
     if (!isDone) {
         return (
-            <div key={id} className={`flex flex-row mt-2 px-1 rounded-full justify-between items-center py-1 peer group border peer ${priorityBGColor} ${classname ? classname : ""}`}>
+            <div key={id} className={`flex flex-row mt-2 px-1 rounded-full justify-between items-center py-1 peer group border peer ${classname ? classname : ""}`}>
                 <div className='text-black flex flex-row justify-start items-center gap-2'>
                     <div id={id} className="p-1 border hover:bg-emerald-400 rounded-full size-8 flex items-center justify-center transition-colors hover:cursor-pointer hover:border-none" onClick={onclick}>
                         <Check color="white" strokeWidth={4} className="size-5 peer" />
@@ -52,12 +40,9 @@ const Todo = (
                         {title}
                     </span>
                 </div>
-                <div className='hidden flex-row justify-end items-center gap-1 peer-hover:flex group-hover:flex'>
-                        <PriorityDropDwon />
-
-                    <Icon href="" onclick={onEdit} className='p-1 size-8'>
-                        <Pencil className='size-4' />
-                    </Icon>
+                <div className='hidden flex-row justify-end items-center gap-2 peer-hover:flex group-hover:flex'>
+                    {/* <PriorityDropDwon /> */}
+                    <ViewTodoDialog todoId={id} todoText={title} priority={priority} onDelete={onDelete} />
                     <Icon href="" className='p-1 size-8 hover:bg-destructive hover:border-none group/cirlce'>
                         <Trash2 className='size-4 group-hover/cirlce:invert' onClick={onDelete} />
                     </Icon>
@@ -77,9 +62,6 @@ const Todo = (
                     </span>
                 </div>
                 <div className='flex-row justify-end items-center gap-1 hidden peer-hover:flex group-hover:flex'>
-                    <Icon href='' onclick={onEdit} className='p-1 size-8 hover:bg-accent'>
-                        <Pencil className='size-4' />
-                    </Icon>
                     <Icon href='' className='p-1 size-8 hover:bg-destructive hover:border-none group/cirlce'>
                         <Trash2 className='size-4 group-hover/cirlce:invert' onClick={onDelete} />
                     </Icon>
