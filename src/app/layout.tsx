8b62af2dc1a52baee1d6e5dd55ad5c3d9fcb2f48
Kanishk Nagpal <kaynagpal03@gmail.com>
2024-07-11 22:36:36 +0530

    progress bar fix and random

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Sidebar from "@/components/Layout/Sidebar";
-import Image from "next/image";
-import GroupSidebar from '../components/Layout/GroupSidebar';
-import RightSidebar from "@/components/Layout/RightSidebar";
-import { Toaster } from "@/components/ui/toaster"
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "500", "600", "700", "800", "900"],
@@ -23,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`relative gap-2 h-screen overflow-y-hidden flex justify-between items-center bg-background ${poppins.className}`}>
+      <body className={`relative gap-2 h-screen overflow-y-hidden flex justify-between items-center px-4 bg-background ${poppins.className}`}>
         {children}
       </body>
     </html>
